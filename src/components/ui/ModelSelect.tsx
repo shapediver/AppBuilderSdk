13fd4a7bd5d161169e67b69f7a28c29cbd03d10e
Dmitry Rumyantsev <lemtoup@gmail.com>
2023-09-19 14:39:56 +0300

    SS-6249: Fix dynamic session id

--- src/components/ui/ModelSelect.tsx
+++ src/components/ui/ModelSelect.tsx
@@ -7,19 +7,23 @@ import { IconAlertCircle } from "@tabler/icons-react";
 
 const sessionData: {
     [key: string]: {
+        id: string,
         ticket: string,
         modelViewUrl: string
     }
 } = {
 	"Ring": {
+		id: "0",
 		ticket: "5f26762cb6a8f87eaacd2add011634a8b24c38a3b9d390986f1aac30a6b8aa5ecc1c2ee566b962097c7c94f3b62ea0aed75e0392d6fbfc3ca79f94feb6ceeb28b274c01f840a2bdab73a0f305af24a879829578c1fd703e267329475d9586dfe985e57127c96b4-2d6cf150663a7b480f6501d17e325861",
 		modelViewUrl: "https://sdr7euc1.eu-central-1.shapediver.com"
 	},
 	"Bench": {
+		id: "1",
 		ticket: "b6b127d7e06588addc43443617c1eeea7ea316bef7ad1273cdd0c82d67f89b8dd4a67a327037b0a3ba2f52377c7d0e1b2a5657dd245603b0a3771d650ea4fbdd76e8187dc21ed1824063e4041b60a28747ed5a51e48c5e77d0c683bee53fb01fa53255e24a74ae-3a01cf3d24f8366dd64a0e2dfce4d4fc",
 		modelViewUrl: "https://sdeuc1.eu-central-1.shapediver.com"
 	},
 	"Outputs Shelf": {
+		id: "2",
 		ticket: "340ff308354b56f5cd0a631f668d48d934a38187c50ff049a19fd3565d316307cb042aaebfdccde871a81f5552c58c04907686e51cada8e8ea7878cfde011ff9d494a54acd68ccf39d9ecfac98bb6a9a2521fc9711294949c1557365b64bbce9e44d420d1b0a64-e5fb4e0ba6c4d6e047685318325f3704",
 		modelViewUrl: "https://sdr7euc1.eu-central-1.shapediver.com"
 	},
@@ -45,7 +49,7 @@ export default function ModelSelect() {
 
 		for (let i = 0; i < selectedModels.length; i++) {
 			sessionsCreateDto.push({
-				id: "selected_session_" + i,
+				id: "selected_session_" + selectedModels[i].id,
 				ticket: selectedModels[i].ticket,
 				modelViewUrl: selectedModels[i].modelViewUrl,
 				excludeViewports: ["viewport_1"],
@@ -70,6 +74,7 @@ export default function ModelSelect() {
 	// callback for when the value was changed
 	const handleChange = (values: string[]) => {
 		const selectedModels: {
+            id: string,
             ticket: string,
             modelViewUrl: string
         }[] = [];
