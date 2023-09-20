b4301f6c27c3814228357d20a8dcc585766ff55c
Alexander Schiftner <alex@shapediver.com>
2023-09-20 17:42:26 +0200

    next step comments by Alex

--- src/types/context/shapediverSliceUI.ts
+++ src/types/context/shapediverSliceUI.ts
@@ -1,6 +1,17 @@
 import { IExportApi } from "@shapediver/viewer";
 import { IParameterApi } from "@shapediver/viewer/src/interfaces/session/IParameterApi";
 
+
+
+/**
+ * TODO: 
+ * 
+ * 1) change this to the following type:
+ * export type IParameters = { [sessionId: string]: { [parameterId: string]: SdReactParameter<any> } };
+ * 
+ * 2) change all parameter components to **only** take an SdReactParameter as input
+ * 
+ */
 export type IParameters = { [sessionId: string]: { [parameterId: string]: IParameterApi<any> } };
 
 export type IExports = { [sessionId: string]: { [exportId: string]: IExportApi } };
