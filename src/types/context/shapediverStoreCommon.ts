63fa1245151effa2f78e86c2ff3d009ed47daf56
Dmitry Rumyantsev <lemtoup@gmail.com>
2023-09-20 13:16:35 +0300

    SS-6249: Update the Viewer version

--- src/types/context/shapediverStoreCommon.ts
+++ src/types/context/shapediverStoreCommon.ts
@@ -1,24 +1,12 @@
 import { IExportApi } from "@shapediver/viewer";
 import { IParameterApi } from "@shapediver/viewer/src/interfaces/session/IParameterApi";
 import { StateCreator, StoreMutatorIdentifier } from "zustand/esm";
+import {
+	SessionCreationDefinition
+} from "@shapediver/viewer.main.creation-control-center/src/interfaces/ICreationControlCenter";
 
-export type SessionCreateDto = {
-	// The ticket for direct embedding of the model to create a session for. This identifies the model on the Geometry Backend.
-	ticket: string,
-	// The modelViewUrl of the ShapeDiver Geometry Backend hosting the model.
-	modelViewUrl: string,
-	// The JWT to use for authorizing the API calls to the Geometry Backend.
-	jwtToken?: string,
-	// The unique identifier to use for the session.
+export interface SessionCreateDto extends SessionCreationDefinition {
 	id: string,
-	// Option to wait for the outputs to be loaded, or return immediately after creation of the session. (default: true)
-	waitForOutputs?: boolean,
-	// Option to load the outputs, or not load them until the first call of customize. (default: true)
-	loadOutputs?: boolean,
-	// Option to exclude some viewports from the start.
-	excludeViewports?: string[],
-	// The initial set of parameter values to use. Map from parameter id to parameter value. The default value will be used for any parameter not specified.
-	initialParameterValues?: { [key: string]: string }
 }
 
 export type SetterFn<T> = (state: T) => T | Partial<T>;
