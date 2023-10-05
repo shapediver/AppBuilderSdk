bee197ce206bce4ef869f097276497ba365526fa
Dmitry Rumyantsev <lemtoup@gmail.com>
2023-10-05 15:31:10 +0300

    SS-7076: Optimize export label

--- src/components/shapediver/exports/ExportLabelComponent.tsx
+++ src/components/shapediver/exports/ExportLabelComponent.tsx
@@ -1,5 +1,5 @@
 import { Skeleton, Text } from "@mantine/core";
-import React, { JSX, useEffect, useState } from "react";
+import React, { JSX } from "react";
 import { ISdReactExport } from "../../../types/shapediver/export";
 
 interface Props {
@@ -13,23 +13,10 @@ interface Props {
  * @returns
  */
 export default function ExportLabelComponent({ exp }: Props): JSX.Element {
-	const [loading, setLoading] = useState(true);
-	const [element, setElement] = useState(<></>);
-
-	useEffect(() => {
-		// early return if the export is not it the store (yet)
-		if (!exp) return;
-
-		// deactivate the loading mode
-		setLoading(false);
-
-		setElement(<Text style={{ paddingBottom: "0.25rem" }} size="sm" fw={500}>{exp.definition.displayname || exp.definition.name}</Text>);
-	}, [exp]);
-
 	return (
 		<>
-			{loading && <Skeleton height={8} mt={6} radius="xl" />}
-			{!loading && element}
+			{!exp && <Skeleton height={8} mt={6} radius="xl" />}
+			{exp && <Text style={{ paddingBottom: "0.25rem" }} size="sm" fw={500}>{exp.definition.displayname || exp.definition.name}</Text>}
 		</>
 	);
 }
