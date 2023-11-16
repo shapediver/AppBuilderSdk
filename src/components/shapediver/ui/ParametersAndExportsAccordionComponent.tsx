b6a902eb0bc929b8cf00a4a150edab7fd894eb13
Dmitry Rumyantsev <lemtoup@gmail.com>
2023-11-16 16:21:43 +0400

    SS-7177: Refactor root layout

--- src/components/shapediver/ui/ParametersAndExportsAccordionComponent.tsx
+++ src/components/shapediver/ui/ParametersAndExportsAccordionComponent.tsx
@@ -48,7 +48,7 @@ export default function ParametersAndExportsAccordionComponent({ parameters, exp
 
 	// as long as there are no parameters, show a loader
 	if (sortedParamsAndExports.length === 0) {
-		return(<Loader className={classes.loader} size="xl" variant="dots" />);
+		return(<section className={classes.loader}><Loader size="xl" variant="dots" /></section>);
 	}
 
 	// loop through the parameters and store the created elements in the elementGroups
