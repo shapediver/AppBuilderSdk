004be52535e092dd1d0de2f57a78efea3b3a8464
Alexander Schiftner <alex@shapediver.com>
2024-03-12 16:52:14 +0100

    SS-7403 Set up appbuilder.shapediver.com (#57)
    * build and deploy script
    
    * report web vitals to console
    
    * deployment prefix
    
    * README fix

---
+++ scripts/build-appbuilder.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+version=$1
+if [ -z "$version" ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+prefix=v1/main
+
+vite build --base=/$prefix/$version/
+if [ $? -ne 0 ]; then
+    echo "Build failed."
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "Skipping deployment."
+    exit 0
+fi
+
+if [ -z "$APPBUILDER_BUCKET" ]; then
+    echo "APPBUILDER_BUCKET environment variable is not set."
+    exit 1
+fi
+
+aws s3 sync ./dist s3://$APPBUILDER_BUCKET/appbuilder/$prefix/$version/ --region us-east-1
+touch empty
+aws s3 cp empty s3://$APPBUILDER_BUCKET/appbuilder/$prefix/$version --region us-east-1 \
+    --website-redirect https://appbuilder.shapediver.com/$prefix/$version/
+rm empty
