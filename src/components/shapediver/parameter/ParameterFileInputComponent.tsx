e271101b8a2df400851ebc1e37f47b80a5064963
Alexander Schiftner <alex@shapediver.com>
2024-03-19 12:05:31 +0100

    display filename if exists

--- src/components/shapediver/parameter/ParameterFileInputComponent.tsx
+++ src/components/shapediver/parameter/ParameterFileInputComponent.tsx
@@ -1,10 +1,11 @@
 import { FileInput } from "@mantine/core";
 import { IconUpload } from "@tabler/icons-react";
-import React from "react";
+import React, { useEffect } from "react";
 import { extendMimeTypes, mapMimeTypeToFileEndings } from "@shapediver/viewer.utils.mime-type";
 import ParameterLabelComponent from "components/shapediver/parameter/ParameterLabelComponent";
 import { PropsParameter } from "types/components/shapediver/propsParameter";
 import { useParameterComponentCommons } from "hooks/shapediver/parameters/useParameterComponentCommons";
+import { isFileParameter } from "types/shapediver/viewer";
 
 /**
  * Functional component that creates a file input for a file parameter.
@@ -20,11 +21,24 @@ export default function ParameterFileInputComponent(props: PropsParameter) {
 		handleChange,
 		onCancel,
 		disabled
-	} = useParameterComponentCommons<File>(props, 0, () => "" /* TODO implement case if default value of File parameter is a string */);
+	} = useParameterComponentCommons<File>(props, 0);
 
 	// create the file endings from all the formats that are specified in the parameter
 	const fileEndings = [...mapMimeTypeToFileEndings(extendMimeTypes(definition.format!))];
 
+	// create a pseudo file in case the value is a file id and a filename for it exists
+	const [ defaultFile, setDefaultFile ] = React.useState<File | null>(null);
+	useEffect(() => {
+		if (typeof(value) === "string" && value.length > 0 && isFileParameter(definition)) {
+			definition.getFilename(value)
+				.then(filename => setDefaultFile(new File([], filename ?? "(Filename unknown)")))
+				.catch(error => console.error(`Error getting filename for file with id ${value}`, error));
+		}
+		else {
+			setDefaultFile(null);
+		}
+	}, [value]);
+	
 	return <>
 		<ParameterLabelComponent { ...props } cancel={onCancel} />
 		{ definition && <FileInput
@@ -34,7 +48,7 @@ export default function ParameterFileInputComponent(props: PropsParameter) {
 			onChange={v => handleChange(v || "")}
 			leftSection={<IconUpload size={14} />}
 			disabled={disabled}
-			value={typeof(value) === "string" ? null : value}
+			value={typeof(value) === "string" ? (value === definition.defval ? defaultFile : null) : value}
 		/> }
 	</>;
 }
