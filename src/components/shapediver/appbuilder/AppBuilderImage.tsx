4817cb1fd3eccab96d65c9310fd96b75bc0eb55b
Dmitry Rumyantsev <lemtoup@gmail.com>
2024-04-11 16:59:59 +0200

    SS-7556: Add the Anchor to images

--- src/components/shapediver/appbuilder/AppBuilderImage.tsx
+++ src/components/shapediver/appbuilder/AppBuilderImage.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Image, ImageProps, MantineThemeComponent, Paper, useProps } from "@mantine/core";
-import { AppBuilderContainerTypeEnum } from "types/shapediver/appbuilder";
+import { Image, ImageProps, MantineThemeComponent, Paper, useProps, Anchor } from "@mantine/core";
+import { AppBuilderContainerTypeEnum, IAppBuilderWidgetPropsAnchor } from "types/shapediver/appbuilder";
 
-interface Props {
+interface Props extends IAppBuilderWidgetPropsAnchor{
 	/** Type of container */
 	containerType?: AppBuilderContainerTypeEnum
 }
@@ -23,8 +23,15 @@ export function AppBuilderImageThemeProps(props: AppBuilderImageThemePropsType):
 }
 
 export default function AppBuilderImage(props: SomeImageProps & Props ) {
-	const { containerType, ...rest } = props;
+	const { containerType, anchor, target, ...rest } = props;
 	const { radius, fit } = useProps("AppBuilderImage", defaultStyleProps, rest);
+	const componentImage = <Image
+		{...rest}
+		fit={fit}
+		radius={radius}
+		h={containerType === AppBuilderContainerTypeEnum.Row ? "100%" : undefined}
+		w={containerType === AppBuilderContainerTypeEnum.Column ? "100%" : undefined}
+	/>;
 
 	return <Paper
 		h={containerType === AppBuilderContainerTypeEnum.Row ? "100%" : undefined}
@@ -35,12 +42,6 @@ export default function AppBuilderImage(props: SomeImageProps & Props ) {
 		pb={0}
 		pl={0}
 	>
-		<Image 
-			{...rest} 
-			fit={fit}
-			radius={radius}
-			h={containerType === AppBuilderContainerTypeEnum.Row ? "100%" : undefined}
-			w={containerType === AppBuilderContainerTypeEnum.Column ? "100%" : undefined}
-		/>
+		{ anchor ? <Anchor href={anchor} target={target}>{componentImage}</Anchor> : componentImage }
 	</Paper>;
 }
