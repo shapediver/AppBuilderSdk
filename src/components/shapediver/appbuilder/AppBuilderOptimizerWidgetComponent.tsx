7fbf1675dacda56425799f7727d51001ee2acc65
mathieu <mathieu@shapediver.com>
2024-04-20 21:33:45 +0200

    modifications

--- src/components/shapediver/appbuilder/AppBuilderOptimizerWidgetComponent.tsx
+++ src/components/shapediver/appbuilder/AppBuilderOptimizerWidgetComponent.tsx
@@ -1,15 +1,13 @@
 import { ShapeDiverResponseParameter } from "@shapediver/sdk.geometry-api-sdk-v2";
 import { AppBuilderSettingsContext } from "context/AppBuilderContext";
-import { Paper, Title, List, Button} from "@mantine/core";
+import { Paper, Title, Checkbox, Button, Switch, Slider, Text} from "@mantine/core";
 import { useSessionPropsParameter } from "hooks/shapediver/parameters/useSessionPropsParameter";
 import { useSortedParametersAndExports } from "hooks/shapediver/parameters/useSortedParametersAndExports";
 import { useOutputContent } from "hooks/shapediver/viewer/useOutputContent";
 import { IObjectiveOutputData, OBJECTIVE_OUTPUT_NAME, ShapeDiverModelOptimizerNsga2 } from "optimization/optimizer";
 import AlertPage from "pages/misc/AlertPage";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { IAppBuilderWidgetPropsOptimizer } from "types/shapediver/appbuilder";
-
-import ParameterSliderComponent from "components/shapediver/parameter/ParameterSliderComponent";
 
 
 interface Props extends IAppBuilderWidgetPropsOptimizer {
@@ -44,34 +42,94 @@ export default function AppBuilderOptimizerWidgetComponent(props: Props) {
 		defaultValues[p.definition.id] = (p.definition as ShapeDiverResponseParameter).defval;
 	});
 
-	const runOptimizer = async () => {
+	const [populationSize, setPopulationSize] = useState(10);
+	const [maxGenerations, setMaxGenerations] = useState(10);
+	const [checkboxValues, setCheckboxValues] = useState([true, true]);
+
+	const runOptimizer = async (values) => {
 		const optimizer = await ShapeDiverModelOptimizerNsga2.create({sessionDto, defaultParameterValues: defaultValues});
-		optimizer.optimize({
-			parameterIds: [],
+		console.log("Running optimizer with values:", values);
+		const result = await optimizer.optimize({
+			parameterIds: undefined,
 			optimizerProps: {
-				populationSize: 10,
-				maxGenerations: 10,
+				populationSize: populationSize,
+				maxGenerations: maxGenerations,
 			}
 		});
+		
 	};
 
+	const [isVisible, setIsVisible] = useState(false);
+	
+	const handleToggle = (event) => {
+		setIsVisible(event.currentTarget.checked);
+	  };
+
+	const handleCheckboxChange = (index) => {
+		const newValues = [...checkboxValues];
+		newValues[index] = !newValues[index];
+		setCheckboxValues(newValues);
+	};
 
 	return <>
-	
+
 		<Paper>
-			<Title order={2}>"Goals to optimize</Title>
+			<Title order={2}>Goals to optimize</Title>
 
-			<List>
-				{Object.entries(objectives).map(([key, value], index) => (
-					<List.Item key={index}>
-						{key}: {value}
-					</List.Item>
-				))}
-			</List>
 
-			<Button onClick={runOptimizer} variant="filled">Run Optimization</Button>
+			{Object.entries(objectives).map(([key, value], index) => (
+
+				<Checkbox
+					defaultChecked
+					label={key}
+					onChange={() => handleCheckboxChange(index)}
+				/>
+
+			))}
+
+			<Button onClick={() => runOptimizer(checkboxValues)} variant="filled">Run Optimization</Button>
+
+			<Switch
+				label="Avanced options"
+				checked={isVisible}
+				onChange={handleToggle}
+			/>
 
 		</Paper>
+
+		{isVisible && (
+			<Paper>
+				<Title order={2}>Optimization options</Title>
+				<Text>Population size</Text>
+				<Slider
+					label={populationSize}
+					min={1}
+					max={100}
+					value={populationSize}
+					onChange={setPopulationSize}
+					showLabelOnHover={false}
+					marks={[
+					{ value: 1, label: '1' },
+					{ value: 100, label: '100' }
+					]}
+				/>
+
+				<Text>Maximum number of generations</Text>
+				<Slider
+					label={maxGenerations}
+					min={1}
+					max={100}
+					value={maxGenerations}
+					onChange={setMaxGenerations}
+					showLabelOnHover={false}
+					marks={[
+					{ value: 1, label: '1' },
+					{ value: 100, label: '100' }
+					]}
+				/>
+
+			</Paper>
+		)}
 	</>;
 		
 }
