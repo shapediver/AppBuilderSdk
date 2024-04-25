745a7bca66bda444170075c2cd442d1577a68e73
Alexander Schiftner <alex@shapediver.com>
2024-04-25 11:24:17 +0200

    guess missing File MIME type

--- src/components/shapediver/parameter/ParameterFileInputComponent.tsx
+++ src/components/shapediver/parameter/ParameterFileInputComponent.tsx
@@ -1,11 +1,36 @@
 import { FileInput } from "@mantine/core";
 import { IconUpload } from "@tabler/icons-react";
 import React, { useEffect } from "react";
-import { extendMimeTypes, mapMimeTypeToFileEndings } from "@shapediver/viewer.utils.mime-type";
+import { extendMimeTypes, guessMimeTypeFromFilename, mapMimeTypeToFileEndings } from "@shapediver/viewer.utils.mime-type";
 import ParameterLabelComponent from "components/shapediver/parameter/ParameterLabelComponent";
 import { PropsParameter } from "types/components/shapediver/propsParameter";
 import { useParameterComponentCommons } from "hooks/shapediver/parameters/useParameterComponentCommons";
 import { isFileParameter } from "types/shapediver/viewer";
+
+/**
+ * Guess the mime type of a file by its extension.
+ * @param fileName 
+ * @returns 
+ */
+const guessMimeTypeByExt = (fileName: string) => {
+	return guessMimeTypeFromFilename(fileName)[0];
+};
+
+/**
+ * In case a file is missing a mime type, try to guess it from the file name.
+ * @param file 
+ * @returns 
+ */
+const guessMissingMimeType = (file: File | string) : File | string => {
+	if (typeof(file) === "string") {
+		return file;
+	}
+	if (file.type) {
+		return file;
+	}
+
+	return new File([file], file.name, { type: guessMimeTypeByExt(file.name) });
+};
 
 /**
  * Functional component that creates a file input for a file parameter.
@@ -45,7 +70,7 @@ export default function ParameterFileInputComponent(props: PropsParameter) {
 			placeholder="File Upload"
 			accept={fileEndings.join(",")}
 			clearable={!!state.execValue}
-			onChange={v => handleChange(v || "")}
+			onChange={v => handleChange(guessMissingMimeType(v || ""))}
 			leftSection={<IconUpload size={14} />}
 			disabled={disabled}
 			value={typeof(value) === "string" ? (value === definition.defval ? defaultFile : null) : value}
