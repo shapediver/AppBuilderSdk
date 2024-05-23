cb694ee37e940843ed8c26d1c4d8ecb11e11916f
Alexander Schiftner <alex@shapediver.com>
2024-05-23 21:29:33 +0200

    set cache-control header

--- scripts/build-appbuilder.sh
+++ scripts/build-appbuilder.sh
@@ -24,8 +24,13 @@ if [ -z "$APPBUILDER_BUCKET" ]; then
     exit 1
 fi
 
-aws s3 sync ./dist s3://$APPBUILDER_BUCKET/appbuilder/$prefix/$version/ --region us-east-1
+cachecontrol="public, max-age=31536000, immutable"
+if [ $version == "latest" ]; then
+    cachecontrol="public, max-age=0, s-maxage=86400"
+fi
+
+aws s3 sync ./dist s3://$APPBUILDER_BUCKET/appbuilder/$prefix/$version/ --region us-east-1 --cache-control "$cachecontrol"
 touch empty
 aws s3 cp empty s3://$APPBUILDER_BUCKET/appbuilder/$prefix/$version --region us-east-1 \
-    --website-redirect https://appbuilder.shapediver.com/$prefix/$version/
+    --website-redirect https://appbuilder.shapediver.com/$prefix/$version/ --cache-control "$cachecontrol"
 rm empty
