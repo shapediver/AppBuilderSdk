5e669a2c6f96cca62442d6f8a6aef4534398af16
Alexander Schiftner <alex@shapediver.com>
2024-05-28 19:28:55 +0200

    move platform util

--- src/hooks/shapediver/appbuilder/useAppBuilderSettings.ts
+++ src/hooks/shapediver/appbuilder/useAppBuilderSettings.ts
@@ -4,39 +4,7 @@ import { IAppBuilderSettings, IAppBuilderSettingsJson, IAppBuilderSettingsSessio
 import useResolveAppBuilderSettings from "./useResolveAppBuilderSettings";
 import { validateAppBuilderSettingsJson } from "types/shapediver/appbuildertypecheck";
 import { useThemeOverrideStore } from "store/useThemeOverrideStore";
-
-const PROD_PLATFORM_HOST = "shapediver.com";
-const STAGING_PLATFORM_HOST = "staging-wwwcdn.us-east-1.shapediver.com";
-const DEV_PLATFORM_HOST = "dev-wwwcdn.us-east-1.shapediver.com";
-const SANDBOX_PLATFORM_HOST = "sandbox-wwwcdn.us-east-1.shapediver.com";
-
-/**
- * Get the default platform URL based on the current hostname.
- * @returns 
- */
-export function getDefaultPlatformUrl() {
-	if (isRunningInPlatform())
-		return origin;
-
-	return `https://${PROD_PLATFORM_HOST}`;
-}
-
-/**
- * Test whether the application is running embedded in the ShapeDiver platform. 
- */
-export function isRunningInPlatform() {
-	const hostname = window.location.hostname;
-	if (hostname === STAGING_PLATFORM_HOST || hostname === "staging-spa.us-east-1.shapediver.com")
-		return true;
-	else if (hostname === DEV_PLATFORM_HOST || hostname === "dev-spa.us-east-1.shapediver.com")
-		return true;
-	else if (hostname === SANDBOX_PLATFORM_HOST || hostname === "sandbox-spa.us-east-1.shapediver.com")
-		return true;
-	else if (hostname === PROD_PLATFORM_HOST || hostname === "www.shapediver.com")
-		return true;
-
-	return false;
-}
+import { getDefaultPlatformUrl } from "utils/shapediver";
 
 /**
  * Test a string value for being "true" or "1".
