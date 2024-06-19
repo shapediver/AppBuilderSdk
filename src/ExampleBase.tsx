83a7230a88626513435d60fd798245ea1e178147
Alexander Schiftner <alex@shapediver.com>
2024-06-19 20:58:29 +0200

    SS-7731 charts styling fixed

--- src/ExampleBase.tsx
+++ src/ExampleBase.tsx
@@ -1,5 +1,6 @@
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
+import "@mantine/charts/styles.css";
 import { MantineProvider } from "@mantine/core";
 import React, { useEffect } from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
