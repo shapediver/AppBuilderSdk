6bac3dfd9b483febeb5e5f849dfa91c970293d14
Michael Oppitz <michael@shapediver.com>
2024-10-03 13:07:27 +0200

    SS-8064: improved AppBuilder bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,42 @@ export default defineConfig({
 		port: 3000,
 	},
 	build: {
+		minify: "terser",
+		terserOptions: {
+			compress: {
+				// Aggressive compression options
+				passes: 3,                 // Number of passes to compress the code
+				drop_debugger: true,       // Remove debugger statements
+				hoist_funs: true,          // Hoist function declarations
+				hoist_vars: true,          // Hoist variable declarations
+				reduce_funcs: true,        // Inline functions when beneficial
+				reduce_vars: true,         // Inline variables when beneficial
+				sequences: true,           // Combine consecutive statements using the comma operator
+				toplevel: true,            // Optimize top-level variable and function scopes
+				unsafe: true,              // Enable optimizations that are unsafe for certain code patterns
+				unsafe_comps: true,        // Optimizations for comparison operators
+				unsafe_math: true,         // Optimizations for math functions
+				unsafe_arrows: true,       // Convert functions to arrow functions
+				unsafe_proto: true,        // Optimize object prototypes
+				unsafe_undefined: true,    // Substitute `undefined` with void 0
+			},
+			mangle: {
+				toplevel: true,             // Mangle top-level function and variable names
+				safari10: true,             // For Safari 10 compatibility
+				properties: {
+					regex: /./,               // Mangle all property names (aggressive)
+					reserved: [],             // No reserved properties; mangle everything
+				},
+			},
+			output: {
+				comments: false,           // Remove comments
+				beautify: false            // Do not beautify the output
+			},
+			safari10: true,              // Support for Safari 10
+			module: true,                // Optimize for ES6 modules
+			keep_classnames: false,      // Drop class names
+			keep_fnames: false           // Drop function names
+		},
 		rollupOptions: {
 			input: {
 				appbuilder: resolve(__dirname, "index.html"),
