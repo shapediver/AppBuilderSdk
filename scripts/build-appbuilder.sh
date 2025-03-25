e235e6fc8b386d2336ea9e55487a17691cfded4b
Michael Oppitz <michael@shapediver.com>
2025-03-25 15:48:44 +0100

    SS-8540: publishing unification

--- scripts/build-appbuilder.sh
+++ scripts/build-appbuilder.sh
@@ -3,6 +3,81 @@ SENTRY_CLI="node_modules/.bin/sentry-cli"
 SENTRY_ORG="shapediver"
 SENTRY_PROJECT="app-builder"
 
+# Function to build and deploy the app
+# $1: deploy - should we deploy the build?
+# $2: prefix - the prefix for the deployment, either "v1/main" or "app/builder/v1/main"
+# $3: version - the version of the deployment (branch name or npm version)
+# $4: deploying_branch - 1 if we are deploying a branch, 0 if we are deploying a version
+build_and_deploy() {
+    deploy=$1
+    prefix=$2
+    version=$3
+    deploying_branch=$4
+
+    # Check if sentryconfig.local.ts exists, copy it to sentryconfig.ts if it does
+    build_timestamp=$(date +'%Y-%m-%d_%H:%M')
+    if [ -f "sentryconfig.local.ts" -a ! -z "${deploy}" ]; then
+        echo "Configuring sentry for build timestamp: ${build_timestamp}"
+        mv sentryconfig.ts sentryconfig.ts.bak
+        sed -e "s/BUILD_TIMESTAMP/${build_timestamp}/" sentryconfig.local.ts > sentryconfig.ts
+    fi
+
+    echo "Building AppBuilder version $version with prefix $prefix"
+    vite build --base=$prefix/$version/
+    if [ $? -ne 0 ]; then
+        echo "Build failed."
+        exit 1
+    fi
+
+    if [ -z "${deploy}" ]; then
+        echo "Skipping deployment."
+        exit 0
+    fi
+
+    if [ -z "$APPBUILDER_BUCKET" ]; then
+        echo "APPBUILDER_BUCKET environment variable is not set."
+        exit 1
+    fi
+
+    cachecontrol="public, max-age=31536000, immutable"
+    # if we are deploying a branch, we need to change the cache control, compared to a version
+    if [ $deploying_branch -eq 1 ]; then
+        cachecontrol="public, max-age=0, s-maxage=86400"
+    fi
+
+    echo "Deploying to version $version with prefix $prefix to bucket $APPBUILDER_BUCKET"
+
+    # depending on the prefix, we need to deploy to different locations
+    if [ $prefix == "v1/main" ]; then
+        aws s3 sync ./dist s3://$APPBUILDER_BUCKET/appbuilder/$prefix/$version/ --region us-east-1 --cache-control "$cachecontrol"
+        touch empty
+        aws s3 cp empty s3://$APPBUILDER_BUCKET/appbuilder/$prefix/$version --region us-east-1 \
+            --website-redirect https://appbuilder.shapediver.com/$prefix/$version/ --cache-control "$cachecontrol"
+        rm empty
+    else
+        aws s3 sync ./dist s3://$APPBUILDER_BUCKET/$prefix/$version/ --region us-east-1 --cache-control "$cachecontrol"
+        touch empty
+        aws s3 cp empty s3://$APPBUILDER_BUCKET/$prefix/$version --region us-east-1 \
+            --website-redirect https://www.shapediver.com/$prefix/$version/ --cache-control "$cachecontrol"
+        rm empty
+    fi
+
+    # Create a sentry release
+    sentry_release="${version}+${build_timestamp}"
+    $SENTRY_CLI releases new -p $SENTRY_PROJECT --org $SENTRY_ORG $sentry_release
+    if [ $? -ne 0 ]; then
+        echo "Failed to create a Sentry release."
+        exit 1
+    fi
+
+    # Associate commits with the release
+    $SENTRY_CLI releases set-commits --org $SENTRY_ORG --auto $sentry_release
+    if [ $? -ne 0 ]; then
+        echo "Failed to associate commits with the Sentry release."
+        exit 1
+    fi
+}
+
 # load environment variables from .env file
 if [ -f .env ]; then
   export $(grep -v '^#' .env | sed 's/#.*//' | sed 's/^ *//;s/ *$//' | xargs)
@@ -18,16 +93,12 @@ deploy=$1
 prefix=$2
 
 # only allow specific prefixes
-if [ -z "$prefix" ]; then
-    echo "Prefix is not set."
-    exit 1
-elif [ $prefix == "app/builder/v1/main" ]; then
+if [ "$prefix" == "app/builder/v1/main" ]; then
     echo "Building for app builder."
-elif [ $prefix == "v1/main" ]; then
+elif [ "$prefix" == "v1/main" ]; then
     echo "Building for app builder platform."
 else
-    echo "Unknown prefix."
-    exit 1
+    echo "No prefix specified, deploying to both app builder and app builder platform."
 fi
 
 # Check if sentry-cli exists
@@ -105,65 +176,10 @@ else
     exit 1
 fi
 
-# Check if sentryconfig.local.ts exists, copy it to sentryconfig.ts if it does
-build_timestamp=$(date +'%Y-%m-%d_%H:%M')
-if [ -f "sentryconfig.local.ts" -a ! -z "${deploy}" ]; then
-    echo "Configuring sentry for build timestamp: ${build_timestamp}"
-    mv sentryconfig.ts sentryconfig.ts.bak
-    sed -e "s/BUILD_TIMESTAMP/${build_timestamp}/" sentryconfig.local.ts > sentryconfig.ts
-fi
-
-echo "Building AppBuilder version $version with prefix $prefix"
-vite build --base=$prefix/$version/
-if [ $? -ne 0 ]; then
-    echo "Build failed."
-    exit 1
-fi
-
-if [ -z "${deploy}" ]; then
-    echo "Skipping deployment."
-    exit 0
-fi
-
-if [ -z "$APPBUILDER_BUCKET" ]; then
-    echo "APPBUILDER_BUCKET environment variable is not set."
-    exit 1
-fi
-
-cachecontrol="public, max-age=31536000, immutable"
-# if we are deploying a branch, we need to change the cache control, compared to a version
-if [ $deploying_branch -eq 1 ]; then
-    cachecontrol="public, max-age=0, s-maxage=86400"
-fi
-
-echo "Deploying to version $version with prefix $prefix to bucket $APPBUILDER_BUCKET"
-
-# depending on the prefix, we need to deploy to different locations
-if [ $prefix == "v1/main" ]; then
-    aws s3 sync ./dist s3://$APPBUILDER_BUCKET/appbuilder/$prefix/$version/ --region us-east-1 --cache-control "$cachecontrol"
-    touch empty
-    aws s3 cp empty s3://$APPBUILDER_BUCKET/appbuilder/$prefix/$version --region us-east-1 \
-        --website-redirect https://appbuilder.shapediver.com/$prefix/$version/ --cache-control "$cachecontrol"
-    rm empty
+# check if a prefix was specified, then only deploy to that prefix
+if [ -z "$prefix" ]; then
+    build_and_deploy $deploy $prefix $version $deploying_branch
 else
-    aws s3 sync ./dist s3://$APPBUILDER_BUCKET/$prefix/$version/ --region us-east-1 --cache-control "$cachecontrol"
-    touch empty
-    aws s3 cp empty s3://$APPBUILDER_BUCKET/$prefix/$version --region us-east-1 \
-        --website-redirect https://www.shapediver.com/$prefix/$version/ --cache-control "$cachecontrol"
-    rm empty
-fi
-
-# Create a sentry release
-sentry_release="${version}+${build_timestamp}"
-$SENTRY_CLI releases new -p $SENTRY_PROJECT --org $SENTRY_ORG $sentry_release
-if [ $? -ne 0 ]; then
-    echo "Failed to create a Sentry release."
-    exit 1
-fi
-
-# Associate commits with the release
-$SENTRY_CLI releases set-commits --org $SENTRY_ORG --auto $sentry_release
-if [ $? -ne 0 ]; then
-    echo "Failed to associate commits with the Sentry release."
-    exit 1
+    build_and_deploy $deploy "v1/main" $version $deploying_branch
+    build_and_deploy $deploy "app/builder/v1/main" $version $deploying_branch
 fi
