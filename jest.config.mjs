4805b565d70d078a2bd27b7b4ea37630e4f10219
Dmitry Rumyantsev <lemtoup@gmail.com>
2026-05-11 12:36:35 +0300

    SS-9298: Replace jest config to the file

---
+++ jest.config.mjs
@@ -0,0 +1,8 @@
+export default {
+	preset: "ts-jest",
+	testEnvironment: "node",
+
+	testMatch: ["**/*.(test|spec).(ts|tsx)"],
+
+	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
